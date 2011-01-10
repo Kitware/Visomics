eccafbd1bcd18c1a799f36d4bda2833ed9b13df5
Joe Snyder <joe.snyder@kitware.com>
2011-01-10 11:02:55 -0500

    Adding in the XCorrelation (Covariance) calculation.
    This is an additional module that checks in the cross correlation example.

---
+++ Sources/voXCorrel.cpp
@@ -0,0 +1,237 @@
+
+// Qt includes
+#include <QDebug>
+
+// Visomics includes
+#include "voApplication.h"
+#include "voXCorrel.h"
+#include "voTableDataObject.h"
+
+// VTK includes
+#include <vtkAdjacencyMatrixToEdgeTable.h>
+#include <vtkAlgorithm.h>
+#include <vtkArrayToTable.h>
+#include <vtkDataSetAttributes.h>
+#include <vtkDescriptiveStatistics.h>
+#include <vtkDoubleArray.h>
+#include <vtkGraph.h>
+#include <vtkMultiBlockDataSet.h>
+#include <vtkSmartPointer.h>
+#include <vtkStringArray.h>
+#include <vtkTable.h>
+#include <vtkTableToArray.h>
+#include <vtkTableToGraph.h>
+
+#include <vtkRCalculatorFilter.h>
+#include <vtkDelimitedTextReader.h>
+#define VTK_CREATE(name,type) \
+  vtkSmartPointer<type> name = vtkSmartPointer<type>::New();
+
+
+// --------------------------------------------------------------------------
+class voXCorrelPrivate
+{
+public:
+
+  vtkSmartPointer<vtkDescriptiveStatistics> Descriptive;
+  vtkSmartPointer<vtkRCalculatorFilter> XCor;
+};
+
+// --------------------------------------------------------------------------
+// voXCorStatisticsPrivate methods
+
+// --------------------------------------------------------------------------
+// voXCorStatistics methods
+
+// --------------------------------------------------------------------------
+voXCorrel::voXCorrel():
+    Superclass(), d_ptr(new voXCorrelPrivate)
+{
+  Q_D(voXCorrel);
+  
+  d->XCor = vtkSmartPointer<vtkRCalculatorFilter>::New();
+
+  d->Descriptive = vtkSmartPointer<vtkDescriptiveStatistics>::New();
+}
+
+// --------------------------------------------------------------------------
+voXCorrel::~voXCorrel()
+{
+}
+
+// --------------------------------------------------------------------------
+void voXCorrel::setInputInformation()
+{
+  this->addInputType("input", "vtkTable");
+}
+
+// --------------------------------------------------------------------------
+void voXCorrel::setOutputInformation()
+{
+  this->addOutputType("corr", "vtkTable",
+                      "", "",
+                      "voTableView", "Table (Correlation)");
+  
+  this->addOutputType("correlation graph", "vtkGraph",
+                      "voCorrelationGraphView", "Correlation Graph");
+}
+
+// --------------------------------------------------------------------------
+bool voXCorrel::execute()
+{
+  Q_D(voXCorrel);
+
+  vtkTable* table =  vtkTable::SafeDownCast(this->input()->data());
+  if (!table)
+    {
+    qWarning() << "Input is Null";
+    return false;
+    }
+
+  /*// Add request to process all columns
+  d->XCor->ResetRequests();
+  d->XCor->ResetAllColumnStates();
+  for (int i = 1; i < table->GetNumberOfColumns(); ++i)
+    {
+    d->XCor->SetColumnStatus(table->GetColumnName(i), 1);
+    }
+  d->XCor->RequestSelectedColumns();
+  */
+
+  //table->Print(std::cout);
+ vtkSmartPointer<vtkTableToArray> tab = vtkSmartPointer<vtkTableToArray>::New();
+ tab->SetInput(table);
+ table->Print(std::cout);
+
+
+
+  for (int ctr=1; ctr<table->GetNumberOfColumns(); ctr++)
+    {
+    tab->AddColumn(table->GetColumnName(ctr));
+    }
+
+  
+  
+  tab->Update();
+  
+ // tab->GetOutput()->Print(std::cout);
+
+  d->XCor->SetRoutput(1);
+  d->XCor->SetInputConnection(tab->GetOutputPort());
+  d->XCor->PutArray("0", "metabData");
+  d->XCor->SetRscript("correl<-cor(metabData)");
+  d->XCor->GetArray("correl","correl");
+ 
+
+  // Do Cross Correlation
+	d->XCor->Update();
+
+#if 0
+  // Find standard deviations of each column
+  d->Descriptive->ResetRequests();
+  d->Descriptive->ResetAllColumnStates();
+  for (int i = 1; i < table->GetNumberOfcols(); ++i)
+    {
+    d->Descriptive->SetColumnStatus(table->GetColumnName(i), 1);
+    d->Descriptive->RequestSelectedColumns();
+    d->Descriptive->ResetAllColumnStates();
+    }
+
+  // Do descriptive stats
+  d->Descriptive->SetInput(table);
+  d->Descriptive->Update();
+
+  vtkMultiBlockDataSet* dBlock = vtkMultiBlockDataSet::SafeDownCast(d->Descriptive->GetOutputDataObject(1));
+  vtkTable* descriptive = vtkTable::SafeDownCast(dBlock->GetBlock(1));
+  vtkDoubleArray* columnDev = vtkDoubleArray::SafeDownCast(descriptive->GetColumnByName("Standard Deviation"));
+#endif
+
+  vtkArrayData *XCorReturn = vtkArrayData::SafeDownCast(d->XCor->GetOutput());
+  if (!XCorReturn)
+    {
+    std::cout << "Downcast DID NOT work." << std::endl;
+    return 1;
+    }
+
+  // Set up headers for the rows.
+  vtkIdType cols = table->GetNumberOfColumns() - 1;
+  vtkIdType rows = table->GetNumberOfRows();
+  vtkSmartPointer<vtkStringArray> header = vtkSmartPointer<vtkStringArray>::New();
+  header->SetName("header");
+  header->SetNumberOfTuples(cols);
+  for (vtkIdType i = 0; i < cols; ++i)
+    {
+    header->SetValue(i, table->GetColumnName(i + 1));
+    }
+
+  // Extract rotated coordinates
+  if (!XCorReturn->GetArrayByName("correl"))
+    {
+    // We should pop up an error message modal window here and return.  For now, cerr will do
+    std::cerr << "R did not return a valid reponse probably due to memory issues.  Cannot display cross correlation result." << std::endl;
+	return false;
+    }
+
+  vtkSmartPointer<vtkArrayData> XCorProjData = vtkSmartPointer<vtkArrayData>::New();
+  XCorProjData->AddArray(XCorReturn->GetArrayByName("correl"));
+
+  vtkSmartPointer<vtkArrayToTable> XCorProj = vtkSmartPointer<vtkArrayToTable>::New();
+  XCorProj->SetInputConnection(XCorProjData->GetProducerPort());
+  XCorProj->Update();  
+  
+  vtkTable* assess = vtkTable::SafeDownCast(XCorProj->GetOutput());
+  vtkSmartPointer<vtkTable> corr = vtkSmartPointer<vtkTable>::New();
+  corr->AddColumn(header);
+
+  assess->Print(std::cout);
+
+
+  for (vtkIdType c = 0;c < cols-1; ++c)  //c < assess->GetNumberOfColumns()
+    {
+    vtkAbstractArray* col = assess->GetColumn(c);
+    col->SetName(table->GetColumnName(c + 1));
+	corr->Print(std::cout);
+	col->Print(std::cout);
+    corr->AddColumn(col);
+    }
+
+  this->setOutput("corr", new voTableDataObject("corr", corr));
+
+  // Find high correlations to put in graph
+  vtkSmartPointer<vtkTable> sparseCorr = vtkSmartPointer<vtkTable>::New();
+  vtkSmartPointer<vtkStringArray> col1 = vtkSmartPointer<vtkStringArray>::New();
+  col1->SetName("Column 1");
+  vtkSmartPointer<vtkStringArray> col2 = vtkSmartPointer<vtkStringArray>::New();
+  col2->SetName("Column 2");
+  vtkSmartPointer<vtkDoubleArray> valueArr = vtkSmartPointer<vtkDoubleArray>::New();
+  valueArr->SetName("Correlation");
+  for (vtkIdType r = 0; r < rows; ++r)
+    {
+    for (vtkIdType c = r+1; c < rows; ++c)
+      {
+      double val = corr->GetValue(r, c + 1).ToDouble();
+      if (val > 0.1)
+        {
+        col1->InsertNextValue(table->GetColumnName(r + 1));
+        col2->InsertNextValue(table->GetColumnName(c + 1));
+        valueArr->InsertNextValue(val);
+        }
+      }
+    }
+  sparseCorr->AddColumn(col1);
+  sparseCorr->AddColumn(col2);
+  sparseCorr->AddColumn(valueArr);
+
+  // Build the graph
+  vtkSmartPointer<vtkTableToGraph> correlGraphAlg =
+    vtkSmartPointer<vtkTableToGraph>::New();
+  correlGraphAlg->SetInput(sparseCorr);
+  correlGraphAlg->AddLinkVertex("Column 1");
+  correlGraphAlg->AddLinkVertex("Column 2");
+  correlGraphAlg->AddLinkEdge("Column 1", "Column 2");
+  correlGraphAlg->Update();
+
+  this->setOutput(
+      "correlation graph", new voDataObject("correlation graph", correlGraphAlg->GetOutput()));
+  return true;
+}
---
+++ Sources/voXCorrel.h
@@ -0,0 +1,35 @@
+
+#ifndef __voXCorrel_h
+#define __voXCorrel_h
+
+// Qt includes
+#include <QScopedPointer>
+
+// Visomics includes
+#include "voAnalysis.h"
+
+class voXCorrelPrivate;
+
+class voXCorrel : public voAnalysis
+{
+  Q_OBJECT
+public:
+  typedef voAnalysis Superclass;
+  voXCorrel();
+  virtual ~voXCorrel();
+
+protected:
+  virtual void setInputInformation();
+  virtual void setOutputInformation();
+
+  virtual bool execute();
+
+protected:
+  QScopedPointer<voXCorrelPrivate> d_ptr;
+
+private:
+  Q_DECLARE_PRIVATE(voXCorrel);
+  Q_DISABLE_COPY(voXCorrel);
+};
+
+#endif
