6f30374c22097e4b9efe7b5fe2d797a625262f23
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-04-11 16:39:01 -0400

    Add voDelimitedTextImportDialog
    This dialog contains a button box and a tab widget.
    
    The associated tabs are:
     - Import
     - Normalization
     - Missing value and identification/rectification
    
    The "import" tab contains "voDelimitedTextImportWidget" and customize it
    with a "Filtering & Pre-processing" groupbox.

---
+++ Application/Testing/Cpp/voDelimitedTextImportDialogTest.cpp
@@ -0,0 +1,37 @@
+
+// Qt includes
+#include <QApplication>
+#include <QTimer>
+#include <QDebug>
+
+// Visomics includes
+#include <voDelimitedTextImportDialog.h>
+
+// STD includes
+#include <cstdlib>
+
+//-----------------------------------------------------------------------------
+int voDelimitedTextImportDialogTest(int argc, char * argv [])
+{
+  QApplication app(argc, argv);
+
+  Q_INIT_RESOURCE(VisomicsApp);
+
+  // Read file
+  QString filename("/home/jchris/Projects/Bioinformatics/Data/UNC/All_conc_kitware_transposed.csv");
+
+  voDelimitedTextImportDialog dialog;
+  dialog.setFileName(filename);
+  
+  dialog.show();
+
+  QTimer autoExit;
+  if (argc < 2 || QString(argv[1]) != "-I")
+    {
+    QObject::connect(&autoExit, SIGNAL(timeout()), &dialog, SLOT(accept()));
+    autoExit.start(1000);
+    }
+
+  return dialog.exec();
+}
+
