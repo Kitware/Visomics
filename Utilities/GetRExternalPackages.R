506e2d6d94d60f0dbaeb0608b2ed85ef131174fb
Brian Helba <brian.helba@kitware.com>
2011-05-26 18:39:07 -0400

    Install script for R external packages

---
+++ Utilities/GetRExternalPackages.R
@@ -0,0 +1,21 @@
+# run: R CMD BATCH GetRExternalPackages.R
+# note: recommend that this be run with administrative/root privileges
+
+is.installed <- function(mypkg) is.element(mypkg, installed.packages()[,1])
+
+write("Checking for R package 'pls'...", file = "")
+if( !is.installed("pls") ) {
+  write("  Package 'pls' not found; installing...", file = "")
+  install.packages(pkgs="pls", repos="http://cran.r-project.org")
+} else {
+  write("  Package 'pls' found", file = "")
+}
+
+write("Checking for R package 'preprocessCore'...", file = "")
+if( !is.installed("preprocessCore") ) {
+  write("  Package 'preprocessCore' not found; installing...", file = "")
+  source("http://www.bioconductor.org/biocLite.R")
+  biocLite("preprocessCore")
+} else {
+  write("  Package 'preprocessCore' found", file = "")
+  }
