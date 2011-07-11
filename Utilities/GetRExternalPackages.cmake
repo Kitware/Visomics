16a8c2a6bc03f1faea70a50324e88961e4828a17
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-07-11 18:32:21 -0400

    GetRExternalPackages.{R, cmake} - Fix indent and update comments

--- Utilities/GetRExternalPackages.cmake
+++ Utilities/GetRExternalPackages.cmake
@@ -1,30 +1,23 @@
 if(NOT R_cmd)
-  message( FATAL_ERROR
-  "ERROR: The variable 'R_COMMAND' is not set."
-  )
+  message(FATAL_ERROR "error: The variable 'R_COMMAND' is not set.")
 endif()
 
-execute_process (
+execute_process(
   COMMAND ${R_cmd} ${R_cmd_args}
   RESULT_VARIABLE return_val
   )
 
 # Print and delete output of R script
 if(EXISTS ${R_output_name})
-  file( READ
-    ${R_output_name}
-    R_output
-    )
-  message( ${R_output} )
-  execute_process(
-    COMMAND ${CMAKE_COMMAND} -E remove ${R_output_name}
-    )
+  file(READ ${R_output_name} R_output)
+  message(${R_output})
+  execute_process(COMMAND ${CMAKE_COMMAND} -E remove ${R_output_name})
 endif()
 
 # If R script fails
-if (NOT "${return_val}" STREQUAL "0")
+if(NOT "${return_val}" STREQUAL "0")
   message( FATAL_ERROR
-  "ERROR: Could not install all external R packages.\n"
+  "error: Could not install all external R packages.\n"
   "Try running \"make RExternalPackages\" as root (sudo)."
   )
 endif()
