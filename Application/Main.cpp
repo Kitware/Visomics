9bc8d0bbd673578f0c3dfc0f4beb450f735d4532
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-09-09 05:08:39 -0400

    Detect when R_HOME is "not set"/"set to nonexistent path"
    * A message will be displayed to the user
    
    * The application will be terminated

--- Application/Main.cpp
+++ Application/Main.cpp
@@ -1,9 +1,45 @@
 
+// Qt includes
+#include <QFile>
+#include <QMessageBox>
+#include <QTimer>
+
+// CTK includes
+#include <ctkCallback.h>
 
 // Visomics includes
 #include "voApplication.h"
+#include "voConfigure.h" // For Visomics_VERSION
 #include "voMainWindow.h"
 
+namespace
+{
+//----------------------------------------------------------------------------
+void checkRHome(void * data)
+{
+  voApplication * app = voApplication::application();
+  if (!QFile::exists(app->rHome()))
+    {
+    QString message("GnuR requires <b>R_HOME</b> environement variable to be set properly.<br><br>");
+    if (app->rHome().isEmpty())
+      {
+      message.append("R_HOME is either not set or set to an empty value.<br>");
+      }
+    else
+      {
+      message.append("R_HOME is set to a nonexistent directory:<br>");
+      message.append("<br>");
+      message.append(app->rHome()).append("<br>");
+      }
+    message.append("<br>The application will be terminated.");
+    QMessageBox::critical(reinterpret_cast<voMainWindow*>(data),
+                          QString("Visomics %1").arg(Visomics_VERSION), message);
+    app->quit();
+    }
+}
+} // end of anonymous namespace
+
+//----------------------------------------------------------------------------
 int main(int argc, char* argv[])
 {
   voApplication app(argc, argv);
@@ -20,6 +56,11 @@ int main(int argc, char* argv[])
   
   voMainWindow mainwindow;
   mainwindow.show();
+
+  ctkCallback checkRHomeCallback;
+  checkRHomeCallback.setCallback(checkRHome);
+  checkRHomeCallback.setCallbackData(&mainwindow);
+  QTimer::singleShot(0, &checkRHomeCallback, SLOT(invoke()));
   
   return app.exec();
 }
