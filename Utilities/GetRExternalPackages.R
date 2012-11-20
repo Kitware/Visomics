be831b51bb5421f54145ca020577f65a319310eb
Brian Helba <brian.helba@kitware.com>
2012-11-20 11:57:15 -0500

    Rewrite GetRExternalPackages.R to be more abstract

--- Utilities/GetRExternalPackages.R
+++ Utilities/GetRExternalPackages.R
@@ -21,30 +21,33 @@
 # Run: R CMD BATCH GetRExternalPackages.R
 # Note: It's recommended to run this script with administrative/root privileges
 
-is.installed <- function(mypkg) is.element(mypkg, installed.packages()[,1])
+is.installed <- function(pkg) is.element(pkg, installed.packages()[,1])
 
-write("Checking for R package 'pls'...", file = "")
-if( !is.installed("pls") ) {
-  write("  Package 'pls' not found; installing...", file = "")
-  install.packages(pkgs="pls", repos="http://cran.r-project.org")
-} else {
-  write("  Package 'pls' found", file = "")
+install.missing.packages <- function(pkgs, install.func) {
+  for(i in 1:length(pkgs)) {
+    pkg <- pkgs[i]
+    if(!is.installed(pkg)) {
+      write(sprintf("  Package '%s' not found; installing...", pkg), file = "")
+      install.func(pkg)
+    } else {
+      write(sprintf("  Package '%s' found", pkg), file = "")
+    }
+  }
 }
 
-write("Checking for R package 'preprocessCore'...", file = "")
-if( !is.installed("preprocessCore") ) {
-  write("  Package 'preprocessCore' not found; installing...", file = "")
-  source("http://www.bioconductor.org/biocLite.R")
-  biocLite("preprocessCore")
-} else {
-  write("  Package 'preprocessCore' found", file = "")
+cran.install <- function(pkg) {
+  install.packages(pkgs = pkg, repos = "http://cran.r-project.org", dependencies = TRUE)
 }
 
-write("Checking for R package 'geiger'...", file = "")
-if( !is.installed("geiger") ) {
-  write("  Package 'geiger' not found; installing...", file = "")
-  install.packages(pkgs="geiger", repos="http://cran.r-project.org",dependencies=TRUE)
-} else {
-  write("  Package 'geiger' found", file = "")
+bioc.install <- function(pkg) {
+  if(!exists("biocLite")) {
+    source("http://www.bioconductor.org/biocLite.R")
+  }
+  biocLite(pkg)
 }
 
+# ##########################################################################
+
+install.missing.packages("pls", cran.install)
+
+install.missing.packages("preprocessCore", bioc.install)
