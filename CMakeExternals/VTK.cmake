Kitware/Visomics

commit 8815d19041f994918479558edcff28f7e62005a9

Updates to work with VTK 6

CMakeExternals/VTK.cmake
@@ -10,7 +10,10 @@ ENDIF()
 SET(proj VTK)
 SET(proj_DEPENDENCIES)
 
-SET(VTK_DEPENDS ${proj})
+SET(VTK_MODULE_ARGS)
+FOREACH(VTK_LIBRARY ${VTK_LIBRARIES})
+  LIST(APPEND VTK_MODULE_ARGS -DModule_${VTK_LIBRARY}:BOOL=ON )
+ENDFOREACH()
 
 IF(NOT DEFINED VTK_DIR)
   MESSAGE(STATUS "Adding external project: ${proj}")
@@ -32,22 +35,18 @@ IF(NOT DEFINED VTK_DIR)
       -DVTK_WRAP_JAVA:BOOL=OFF
       -DVTK_WRAP_PYTHON:BOOL=OFF
       -DVTK_WRAP_TCL:BOOL=OFF
-      # Build features
-      -DVTK_USE_GNU_R:BOOL=ON
-      -DVTK_USE_N_WAY_ARRAYS:BOOL=ON # Required for using R interface
-      -DVTK_USE_INFOVIS:BOOL=ON # Required for using R interface
-      -DVTK_USE_GUISUPPORT:BOOL=ON
-      -DVTK_USE_QVTK_QTOPENGL:BOOL=ON
-      -DVTK_USE_QT:BOOL=ON
-      # Options for R
+      # Modules
+      -DVTK_Group_StandAlone:BOOL=OFF
+      ${VTK_MODULE_ARGS}
+      # Module options - vtkFiltersStatisticsGnuR
       -DR_COMMAND:PATH=${R_COMMAND}
-      -DVTK_R_HOME:PATH=${R_HOME}
+      -DR_HOME:PATH=${R_HOME}
       -DR_INCLUDE_DIR:PATH=${R_INCLUDE_DIR}
       -DR_LIBRARY_BASE:FILEPATH=${R_LIBRARY_BASE}
       -DR_LIBRARY_BLAS:FILEPATH=${R_LIBRARY_BLAS}
       -DR_LIBRARY_LAPACK:FILEPATH=${R_LIBRARY_LAPACK}
       -DR_LIBRARY_READLINE:FILEPATH=${R_LIBRARY_READLINE}
-      # Options for Qt
+      # Module options - vtkViewsQt
       -DQT_QMAKE_EXECUTABLE:FILEPATH=${QT_QMAKE_EXECUTABLE}
     DEPENDS
       ${proj_DEPENDENCIES}

Application/voAboutDialog.cpp
@@ -23,7 +23,7 @@
 #include "voConfigure.h"
 
 // VTK includes
-#include <vtkConfigure.h>
+#include <vtkVersionMacros.h>
 
 // --------------------------------------------------------------------------
 class voAboutDialogPrivate

Application/voDelimitedTextPreviewModel.cpp
@@ -663,7 +663,7 @@ void voDelimitedTextPreviewModel::updatePreview()
 
   // TODO: Add missing value identification/rectification step
   vtkNew<vtkStringToNumeric> numericToStringFilter;
-  numericToStringFilter->SetInput(d->OriginalDataTable);
+  numericToStringFilter->SetInputData(d->OriginalDataTable.GetPointer());
   numericToStringFilter->Update();
   vtkTable * numericDataTable = vtkTable::SafeDownCast(numericToStringFilter->GetOutput());
   Q_ASSERT(numericDataTable);

Base/Analysis/voANOVAStatistics.cpp
@@ -169,7 +169,7 @@ bool voANOVAStatistics::execute()
 
   // Run R code
   d->RCalc->SetRoutput(0);
-  d->RCalc->SetInputConnection(RInputArrayData->GetProducerPort());
+  d->RCalc->SetInputData(RInputArrayData.GetPointer());
   d->RCalc->PutArray("0", "sample1Array");
   d->RCalc->PutArray("1", "sample2Array");
   d->RCalc->GetArray("P-Value","pValue");

Base/Analysis/voFoldChange.cpp
@@ -168,7 +168,7 @@ bool voFoldChange::execute()
 
   // Run R code
   d->RCalc->SetRoutput(0);
-  d->RCalc->SetInputConnection(RInputArrayData->GetProducerPort());
+  d->RCalc->SetInputData(RInputArrayData.GetPointer());
   d->RCalc->PutArray("0", "sample1Array");
   d->RCalc->PutArray("1", "sample2Array");
   d->RCalc->GetArray("Average Initial","avgInit");

Base/Analysis/voHierarchicalClustering.cpp
@@ -131,7 +131,7 @@ bool voHierarchicalClustering::execute()
   // Run R
   vtkNew <vtkRCalculatorFilter> RCalc;
   RCalc->SetRoutput(0);
-  RCalc->SetInputConnection(RInputArrayData->GetProducerPort());
+  RCalc->SetInputData(RInputArrayData.GetPointer());
   RCalc->PutArray("0", "metabData");
   RCalc->GetArray("height","height");
 //  RCalc->GetArray("order","order");

Base/Analysis/voKMeansClustering.cpp
@@ -190,7 +190,7 @@ bool voKMeansClustering::execute()
   // Run R
   vtkNew<vtkRCalculatorFilter> RCalc;
   RCalc->SetRoutput(0);
-  RCalc->SetInputConnection(RInputArrayData->GetProducerPort());
+  RCalc->SetInputData(RInputArrayData.GetPointer());
   RCalc->PutArray("0", "metabData");
   RCalc->GetArray("kmCenters", "kmCenters");
   RCalc->GetArray("kmCluster", "kmCluster");

Base/Analysis/voPCAStatistics.cpp
@@ -122,7 +122,7 @@ bool voPCAStatistics::execute()
     }
 
   d->RCalc->SetRoutput(0);
-  d->RCalc->SetInputConnection(RInputArrayData->GetProducerPort());
+  d->RCalc->SetInputData(RInputArrayData.GetPointer());
   d->RCalc->PutArray("0", "PCAData");
   d->RCalc->GetArray("pcaRot", "pcaRot");
   d->RCalc->GetArray("pcaSdev", "pcaSdev");

commit 9718970a13d9ddc3428fb54f886bca2bb34e64d1

Fix bug in build dependencies
Visomics external project now properly depends on VTK external project.

This was broken by 8815d19041f994918479558edcff28f7e62005a9.

CMakeExternals/VTK.cmake
@@ -10,6 +10,8 @@ ENDIF()
 SET(proj VTK)
 SET(proj_DEPENDENCIES)
 
+SET(VTK_DEPENDS ${proj})
+
 SET(VTK_MODULE_ARGS)
 FOREACH(VTK_LIBRARY ${VTK_LIBRARIES})
   LIST(APPEND VTK_MODULE_ARGS -DModule_${VTK_LIBRARY}:BOOL=ON )