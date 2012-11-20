1f52980110a26822790a702bd2b74ccc4b14836f
Brian Helba <brian.helba@kitware.com>
2012-11-20 12:41:49 -0500

    Make GetRExternalPackages.R be CMake-configured

--- CMakeExternals/RExternalPackages.cmake
+++ CMakeExternals/RExternalPackages.cmake
@@ -3,10 +3,16 @@ SET(target RExternalPackages)
 SET(R_cmd ${R_COMMAND})
 SET(R_cmd_batch CMD BATCH)
 SET(R_options --no-restore --no-save --slave --no-timing)
-SET(R_script_name "${CMAKE_SOURCE_DIR}/Utilities/GetRExternalPackages.R")
+SET(R_script_name "${CMAKE_BINARY_DIR}/GetRExternalPackages.R")
 SET(R_output_name "${CMAKE_BINARY_DIR}/GetExternalPackages.Rout")
 
 SET(R_cmd_args ${R_cmd_batch} ${R_options} -- ${R_script_name} ${R_output_name}) # Concatenate all command arguments for easy passing
+
+configure_file(
+  ${CMAKE_SOURCE_DIR}/Utilities/GetRExternalPackages.R.in
+  ${CMAKE_BINARY_DIR}/GetRExternalPackages.R
+  @ONLY
+)
 
 add_custom_target(${target} ALL
   COMMAND ${CMAKE_COMMAND}
