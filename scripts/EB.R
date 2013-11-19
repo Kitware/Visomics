d8842226165c7b9d203a043d9821f6fe89dcfc79
Chris Harris <chris.harris@kitware.com>
2013-11-19 08:08:31 -0500

    Remove EB analysis
    Early-burst model fitting is now provided as part of the Geiger
    model fitting analysis.

--- scripts/EB.R
+++
@@ -1,7 +0,0 @@
-library(geiger)
-data<-as.numeric(input_table$column_name)
-names(data)<-input_table[[1]]
-o<-fitContinuous(input_tree, data, model="EB",SE=0)
-result=o$opt
-EB_table=list(parameter="value",z0=result$z0,sigsq=result$sigsq,a=result$a," "=" ",lnL=result$lnL,AIC=result$aic,AICc=result$aicc)
-EB_tree<-transform(input_tree, "EB", o$opt$a)
