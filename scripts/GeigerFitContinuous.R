646ac0146a25c87a65fc8f526f90519357315903
Zack Galbreath <zack.galbreath@kitware.com>
2014-04-16 16:22:11 -0400

    update fitContinuous
    update fitContinuous analysis for geiger 2.0

--- scripts/GeigerFitContinuous.R
+++ scripts/GeigerFitContinuous.R
@@ -1,4 +1,4 @@
-library(geiger)
+library("geiger", warn.conflicts=FALSE)
 
 modelName <- "modelType"
 
@@ -9,13 +9,13 @@ result=o$opt
 
 if (modelName == "OU") {
   resultTable=list("value",z0=result$z0,sigsq=result$sigsq,alpha=result$alpha," "=" ",lnL=result$lnL,AIC=result$aic,AICc=result$aicc)
-   resultTree<-transform(tree, "OU", o$opt$alpha)
+   resultTree<-rescale(tree, "OU", o$opt$alpha)
 } else if (modelName == "BM") {
   resultTable=list("value",z0=result$z0,sigsq=result$sigsq," "=" ",lnL=result$lnL,AIC=result$aic,AICc=result$aicc)
   resultTree<-tree
 } else if (modelName == "EB") {
   resultTable=list("value",z0=result$z0,sigsq=result$sigsq,a=result$a," "=" ",lnL=result$lnL,AIC=result$aic,AICc=result$aicc)
-  resultTree<-transform(tree, "EB", o$opt$a)
+  resultTree<-rescale(tree, "EB", o$opt$a)
 }
 
 
